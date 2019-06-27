16575b8da1c501be298706c702c710359ea64527
Pax <pax.margret@gmail.com>
2019-06-26 18:46:37 -0700

    Configure website + install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    extend: {
+      minWidth: {
+        '1/3': '33.33%',
+      }
+    },
+  },
+  variants: {},
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.trans': {
+          transition: 'all .25s',
+        },
+        '.trans-bgc': {
+          transitionProperty: 'background color',
+        }
+      }
+
+      addUtilities(newUtilities)
+    }
+  ]
+}
