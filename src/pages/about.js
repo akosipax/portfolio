b707c191658882fe3c55d568d716177c985c5eb2
Pax <pax.margret@gmail.com>
2019-07-31 21:02:02 -0700

    Modify content; Use global link styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,9 +8,15 @@ import "../css/global.css";
 const AboutPage = () => (
   <Layout title="About me.">
     <SEO title="About" />
-    <p>I'm a Christian, a wife, and a mom living in Abbotsford, B.C., Canada. I'm not good with bios.</p>
-    <p>I grew up in the Philippines and moved to Canada in 2010.</p>
-    <p>I have a B.Sc. Computer Science degree in De La Salle University&ndash;Manila specializing in Software Technology where I experienced building a social media website prototype, making up a programming language, and developing a Q&amp;A system using natural language processing and I'm really good at making long, awful, run-on sentences.</p>
+    
+    <p>I'm a Christian, a wife, and a mom living in Abbotsford, B.C., Canada. I grew up in the Philippines and moved to Canada in 2010.</p>
+    <p>I have a B.Sc. Computer Science degree in De La Salle University&ndash;Manila specializing in Software Technology where I experienced building a social media website prototype, implementing our own programming language, and developing a Q&A system using natural language processing.</p>
+    <p>When I'm not coding, I...</p>
+    <ul className="list-disc list-inside">
+      <li className="mb-0">practice the little Japanese I know over at <a className="linkRegular linkBorder" href="https://www.duolingo.com/akosipax" target="_blank" rel="noopener noreferrer">Duolingo</a></li>
+      <li className="mb-0">hang out with <a  className="linkRegular linkBorder" href="https://scheduler.margret.pw/img/IMG_0746.jpg" target="_blank" rel="noopener noreferrer">my toddler</a></li>
+      <li className="mb-0">read books (currently going through <a className="linkRegular linkBorder" href="https://eloquentjavascript.net/" target="_blank" rel="noopener noreferrer">Eloquent Javascript</a>)</li>
+    </ul>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,23 @@ import SEO from "../components/seo"
 
 import "../css/global.css";
 
-const linkStyle = `text-teal-700  
-  border-dotted border-b 
-  hover:text-teal-900 
-  hover:border-0
-  `
 const IndexPage = () => (
   <Layout title="Hello there.">
     <SEO title="Home" />
-    <p>I&rsquo;m Pax. Welcome to my portfolio! I'm available <a href="mailto:pax.margret@gmail.com" className={linkStyle}>for hire.</a></p>
+    <p>I&rsquo;m Pax. Welcome to my portfolio! I'm available <a className="linkRegular linkBorder" href="mailto:pax.margret@gmail.com">for hire.</a></p>
     <p>If any one of these situations sounds familiar to you, I can help!</p>
     <ul>
-      <li><span role="img" aria-label="sad">😔</span> You know your way around your CMS but you can't figure out how to make it do what you want or look the way you want.</li>
+      <li><span role="img" aria-label="sad">😔</span> You know your way around your website (your WordPress, Drupal, Gatsby, or Laravel website) but you can't figure out how to make it do what you want or look the way you want.</li>
       <li><span role="img" aria-label="distraught">😩</span> Your small business website is <em>bloated</em> and is not as fast as it can be.</li>
-      <li><span role="img" aria-label="please">🙏</span> You know your stuff but you just wish someone who cares about test-driven, clean, and  maintainable code will lend you a hand.</li>
-      <li><span role="img" aria-label="persevering">😣</span> Your app or feature is <em>a drag</em> to maintain and wish someone would write tests for you.</li>
       <li><span role="img" aria-label="excited">🤩</span> You dream of your own single-page application that's fast and SEO-friendly.</li>
     </ul>
+    <p className="mt-16">I also collaborate with development teams. So if...</p>
+    <ul>
+      <li><span role="img" aria-label="please">🙏</span> you need help developing an app or a feature and values clean [test-driven, readable] code</li>
+      <li><span role="img" aria-label="persevering">😣</span> your app or feature is <em>a drag</em> to maintain and wish someone would write tests for you</li>
+      <li><span role="img" aria-label="high-five">✋</span> you'd appreciate a hand with tracking down and squashing bugs</li>
+    </ul>
+    <p>...don't hesitate to <a className="linkRegular linkBorder" href="mailto:pax.margret@gmail.com">contact me.</a></p>
   </Layout>
 )
 
