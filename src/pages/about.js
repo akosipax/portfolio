79100f36b60e0708de9db1f57fd8006704f6f808
Pax <pax.margret@gmail.com>
2019-09-04 01:34:58 -0700

    Fix list in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,9 +13,9 @@ const AboutPage = () => (
     <p>I have a B.Sc. Computer Science degree in De La Salle University&ndash;Manila specializing in Software Technology where I experienced building a social media website prototype, implementing our own programming language, and developing a Q&A system using natural language processing.</p>
     <p>When I'm not coding, I...</p>
     <ul className="list-disc list-inside">
-      <li className="mb-0">practice the little Japanese I know over at <a className="linkRegular linkBorder" href="https://www.duolingo.com/akosipax" target="_blank" rel="noopener noreferrer">Duolingo</a></li>
-      <li className="mb-0">hang out with <a  className="linkRegular linkBorder" href="https://babysleep.app/img/IMG_0746.jpg" target="_blank" rel="noopener noreferrer">my toddler</a></li>
-      <li className="mb-0">read books (currently going through <a className="linkRegular linkBorder" href="https://eloquentjavascript.net/" target="_blank" rel="noopener noreferrer">Eloquent Javascript</a>)</li>
+      <li className="my-0">practice the little Japanese I know over at <a className="linkRegular linkBorder" href="https://www.duolingo.com/akosipax" target="_blank" rel="noopener noreferrer">Duolingo</a></li>
+      <li className="my-0">hang out with <a  className="linkRegular linkBorder" href="https://babysleep.app/img/IMG_0746.jpg" target="_blank" rel="noopener noreferrer">my toddler</a></li>
+      <li className="my-0">read books (currently going through <a className="linkRegular linkBorder" href="https://eloquentjavascript.net/" target="_blank" rel="noopener noreferrer">Eloquent Javascript</a>)</li>
     </ul>
   </Layout>
 )
