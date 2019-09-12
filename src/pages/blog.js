4076f83ac9605ad405c9eb6ba753e31925b74dfc
Pax <pax.margret@gmail.com>
2019-09-11 23:41:09 -0700

    Create a dynamic index of blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,51 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "../css/global.css";
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout title="My blog.">
     <SEO title="Blog of Margret `Pax` Williams" />
-    <div className="blog-post">
-      <h1 className="text-3xl leading-none font-semibold"><a href="/blog/first-python-app-porting-best-practices-over-python">My First Python App</a></h1>
-      <h2 className="italic leading-none mt-2 text-gray-700 text-medium">Porting Best Practices Over to Python</h2>
-      <span className="text-sm" title="Sept. 3, 2019">Sept. 3 ・ 4 min</span>
-      <p className="my-4">For the last month, I’ve been learning Python and building an app along the way. There are already a lot of blogs out there on Flask, web crawling, and Python in general but what I usually find lacking are notes on best practices such as using virtual environments, testing code, and even using sessions responsibly---and understandably so, since they are meant to be *quick* tutorials.</p>
-      <a className="linkRegular" href="/blog/first-python-app-porting-best-practices-over-python">Continue reading...</a>
-    </div>
+    {data.allMarkdownRemark.edges.map(({node}) => (
+      <div key={node.id} className="blog-post">
+        <Link
+          to={node.frontmatter.path}
+          className="text-3xl leading-none font-semibold"
+        >
+          <h1>{node.frontmatter.title}</h1>
+        </Link>
+        <h2 className="italic leading-none mt-2 text-gray-700 text-sm">{node.frontmatter.subtitle}</h2>
+        <span className="text-sm">{node.frontmatter.date} ・ {node.frontmatter.length}</span>
+        <p className="my-4">{node.frontmatter.excerpt}</p>
+        <Link
+          to={node.frontmatter.path}
+          className="linkRegular">Continue reading...</Link>
+      </div>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM D, YYYY")
+            path
+            subtitle
+            length
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`
