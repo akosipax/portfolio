c0eb9a1bd0f71bd121ce82393af33643dc1a6da8
Margret `Pax` Williams <pax.margret@protonmail.com>
2021-01-14 11:15:16 -0800

    feat: update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,21 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 import "../css/global.css";
+
+
 
 const AboutPage = () => (
   <Layout title="About me.">
     <SEO title="About" />
-    
-    <p>I'm a Christian, a wife, and a mom living in Abbotsford, B.C., Canada. I grew up in the Philippines and moved to Canada in 2010.</p>
+
+    <p>I'm a Christian, a wife, and a mom living in the Fraser Valley, Canada. I grew up in the Philippines and moved to Canada in 2010.</p>
     <p>I have a B.Sc. Computer Science degree in De La Salle University&ndash;Manila specializing in Software Technology where I experienced building a social media website prototype, implementing our own programming language, and developing a Q&A system using natural language processing.</p>
     <p>When I'm not coding, I...</p>
     <ul className="list-disc list-inside">
-      <li className="my-0">practice the little Japanese I know over at <a className="linkRegular linkBorder" href="https://www.duolingo.com/akosipax" target="_blank" rel="noopener noreferrer">Duolingo</a></li>
-      <li className="my-0">hang out with <a  className="linkRegular linkBorder" href="https://babysleep.app/img/IMG_0746.jpg" target="_blank" rel="noopener noreferrer">my toddler</a></li>
-      <li className="my-0">read books (currently going through <a className="linkRegular linkBorder" href="https://fvrl.bibliocommons.com/item/show/1796139021" target="_blank" rel="noopener noreferrer">Supercharged Python</a>)</li>
+      <li className="my-0">watch [ Queen's Gambit | Cells at Work | war movies | something-marvel ] with my husband</li>
+      <li className="my-0">hang out with <a className="linkRegular linkBorder" href="https://babysleep.app/img/IMG_0746.jpg" target="_blank" rel="noopener noreferrer">my toddler</a></li>
+      <li className="my-0">read <a className="linkRegular linkBorder" href="https://fvrl.bibliocommons.com/user_profile/97290383" target="_blank" rel="noopener noreferrer">books</a></li>
     </ul>
   </Layout>
 )
