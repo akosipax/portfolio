425ee6141bf891d7553f7f65417150f77cbf093f
Margret `Pax` Williams <pax.margret@protonmail.com>
2021-01-14 10:49:20 -0800

    feat: add buymeacoffee link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import kofi from "../images/BuyMeACoffee_Red@2x.png"
 import github from "../images/github32.png"
 import so from "../images/so-icon.svg"
 
@@ -8,6 +9,9 @@ const Footer = ({ data }) => (
       <a className="linkRegular" href="https://babysleep.app/img/IMG_0746.jpg"> Jeconiah</a>. ♥ Hand-built by yours truly.
       <span role="img" aria-label="shy monkey">🙈</span>
     </p>
+    <a href="https://ko-fi.com/thisispax#paymentModal" aria-label="Buy me a tea!" target="_blank" rel="noopener noreferrer">
+      <img src={kofi} alt="ko-fi icon" className="h-8 inline-block mr-3 my-3" />
+    </a>
     <a href="https://github.com/paxcodes" aria-label="Check out my Github activity" target="_blank" rel="noopener noreferrer">
       <img src={github} alt="github logo" className="h-6 inline-block mr-3 my-3" />
     </a>
