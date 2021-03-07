daea3f6d7b40fadfd2c7a307d4c3d27fc46767b3
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-07 15:33:02 -0800

    fix: remove projects; update others

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,19 +1,18 @@
 import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 import BuiltWith from "../components/builtWith"
+import Layout from "../components/layout"
 import Screenshots from "../components/screenshots"
-
+import SEO from "../components/seo"
 import "../css/global.css"
-import styles from "../css/projects.module.css"
-
-import chromeExtBeforeGIF from "../images/screenshots/chromeext-before.gif"
+import logos from "../images/logos/logos"
 import chromeExtAfterGIF from "../images/screenshots/chromeext-after.gif"
-import psTests from "../images/screenshots/peppsnipp/ps-testscripts.png"
+import chromeExtBeforeGIF from "../images/screenshots/chromeext-before.gif"
 import psFrontEnd1 from "../images/screenshots/peppsnipp/ps-frontend1.png"
 import psFrontEnd3 from "../images/screenshots/peppsnipp/ps-frontend3.png"
-import logos from "../images/logos/logos"
+import psTests from "../images/screenshots/peppsnipp/ps-testscripts.png"
+
+
+
 
 const ProjectsPage = () => (
   <Layout title="My work.">
@@ -51,8 +50,8 @@ const ProjectsPage = () => (
       ]}
     />
     <p>
-      The Pepperplate Snipper exports recipes in your Pepperplate account as
-      JSON and/or PNG screenshots.
+      The Pepperplate Snipper exports recipes in someone's Pepperplate account as
+      JSON and/or PNG screenshots through web-scraping.
     </p>
     <Screenshots
       images={[
@@ -82,9 +81,14 @@ const ProjectsPage = () => (
     <BuiltWith
       items={[
         {
-          href: "https://phpunit.de/",
-          description: "PHPUnit",
-          src: logos.phpunit,
+          href: "https://www.python.org/",
+          description: "Python",
+          src: logos.python,
+        },
+        {
+          href: "https://docs.pytest.org/en/latest/",
+          description: "PyTest",
+          src: logos.pytest,
         },
         {
           href: "http://cypress.io/",
@@ -92,19 +96,10 @@ const ProjectsPage = () => (
           src: logos.cypress,
         },
         {
-          href: "http://laravel.com/",
-          description: "Laravel—a PHP framework",
-          src: logos.laravel,
-        },
-        {
           href: "https://vuejs.org/",
           description: "Vue.js",
           src: logos.vue,
           width: 40,
-        },
-        {
-          href: "https://vuex.vuejs.org/",
-          description: "Vuex",
         },
       ]}
     />
@@ -130,9 +125,12 @@ const ProjectsPage = () => (
       ]}
     />
     <p>
-      A private chrome extension to check whether broadcasts are properly
-      uploaded to SoundCloud. This turned a tedious 15-minute task into
-      something that can be done in a few seconds.
+      A private chrome extension that integrates with the SoundCloud API to check
+      whether broadcasts are properly uploaded to SoundCloud.
+      Before this chrome extension, someone logs into SoundCloud
+      and manually checks 30+ broadcasts one by one to make sure it has the correct
+      permalink and title. The chrome extension turned a tedious, error-prone task into
+      something that can be done accurately in a few seconds.
     </p>
     <Screenshots
       label="animation showing how the extension works"
@@ -147,86 +145,6 @@ const ProjectsPage = () => (
         },
       ]}
     />
-
-    <h3>
-      <a href="https://www.insightforliving.ca">
-        <span role="img" aria-label="link">
-          🔗
-        </span>{" "}
-        Insight for Living Canada
-      </a>
-    </h3>
-    <p>
-      Website for the organization, Insight for Living Canada&mdash;the
-      Bible-teaching ministry of Charles (Chuck) Swindoll.
-    </p>
-    <ul>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Drupal 7
-      </li>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Backend Development
-      </li>
-      <li className={`${styles.item} leading-tight`}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Social Media Metadata <em>for beautiful share previews</em>
-      </li>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        ConstantContact and SoundCloud Integration
-      </li>
-    </ul>
-
-    <h3>
-      <a href="https://www.theshiyrpoets.com">
-        <span role="img" aria-label="link">
-          🔗
-        </span>{" "}
-        The SHIYR Poets
-      </a>
-    </h3>
-    <p>
-      Website for the band, The SHIYR Poets. The SHIYR Poets (pronounced
-      “Sheer”) are Brian Doerksen, Calum Rees, Brian Thiessen and Teresa Trask.
-      They aim to render each psalm in its entirety, singing songs of
-      desperation, desire, justice and praise.
-    </p>
-    <ul>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Drupal 7
-      </li>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Full-Stack Development
-      </li>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Phase 1: Paypal Buttons
-      </li>
-      <li className={styles.item}>
-        <span role="img" aria-label="check">
-          ✅
-        </span>{" "}
-        Phase 2: Drupal Commerce
-      </li>
-    </ul>
   </Layout>
 )
 
