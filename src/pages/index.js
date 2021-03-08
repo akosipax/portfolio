b14bd40de62f5bc202d6494f7e41903a49031b19
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-07 19:09:11 -0800

    fix: typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <p>During times I am not monetarily compensated (a.k.a. &ldquo;free time&rdquo;), I build apps and their backend APIs. The first pinned repo in <a class="linkRegular linkBorder" href="https://github.com/paxcodes">my GitHub</a> is my current project.
     </p>
 
-    <p>Check out <a class="linkRegular linkBorder" href="/projects">projects</a> I've done.</p>
+    <p>Check out <a class="linkRegular linkBorder" href="/projects">other projects</a> I've done.</p>
 
     <p>Or eavesdrop on <a class="linkRegular linkBorder" href="/notes">"conversations with myself"</a> when I attempt to figure out something.</p>
   </Layout>
