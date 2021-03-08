00ddf700f27ee4eea4929b27dfd929de02effa87
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-07 19:05:44 -0800

    fix: add new projects

--- src/images/logos/logos.js
+++ src/images/logos/logos.js
@@ -1,13 +1,14 @@
-import selenium from "./selenium.png"
+import chrome from "./chrome.png"
+import cypress from "./cypress.png"
+import js from "./js.png"
+import laravel from "./laravel.png"
+import phpunit from "./phpunit.png"
 import pytest from "./pytest.png"
 import python from "./python.svg"
 import qt from "./qt.svg"
-import phpunit from "./phpunit.png"
-import cypress from "./cypress.png"
-import laravel from "./laravel.png"
+import selenium from "./selenium.png"
+import typer from "./typer.svg"
 import vue from "./vue.svg"
-import chrome from "./chrome.png"
-import js from "./js.png"
 
 export default {
   selenium,
@@ -20,4 +21,5 @@ export default {
   cypress,
   chrome,
   js,
+  typer,
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -20,7 +20,6 @@ const ProjectsPage = () => (
     <h3>
       Weekly Email Roundup Generator
     </h3>
-    <p>Automatically create and schedule newsletters in Mailchimp using their API; Newsletters contain new articles, videos, and broadcasts published by the organization (collected by parsing RSS feeds); and top posts in the Facebook page  (calculated using Facebook’s API).</p>
     <BuiltWith
       items={[
         {
@@ -34,23 +33,99 @@ const ProjectsPage = () => (
           src: logos.pytest,
         },
         {
-          href: "https://selenium-python.readthedocs.io/",
-          description: "Selenium",
-          src: logos.selenium,
+          href: "https://www.pyinstaller.org/",
+          description: "PyInstaller",
         },
         {
-          href: "https://pypi.org/project/PyQt5/",
-          description: "PyQt5",
-          src: logos.qt,
+          href: "https://typer.tiangolo.com/",
+          description: "Typer",
+          src: logos.typer,
+        },
+      ]}
+    />
+    <p>Automatically create and schedule newsletters in Mailchimp using their API; Newsletters contain new articles, videos, and broadcasts published by the organization (collected by parsing RSS feeds); and top posts in the Facebook page (calculated using Facebook’s API). It is designed to run by crontab on CentOS 7 server every Friday at 1am.</p>
+    <Screenshots
+      images={[
+        {
+          src: psTests,
+          description: "Tests for the Pepperplate Crawler",
+        },
+        {
+          src: psFrontEnd1,
+          description: "Initial process",
+        },
+        {
+          src: psFrontEnd3,
+          description: "Exporting recipes",
+        },
+      ]}
+    />
+    <h3>
+      Weekly Web Report Generator
+    </h3>
+    <BuiltWith
+      items={[
+        {
+          href: "https://www.python.org/",
+          description: "Python",
+          src: logos.python,
+        },
+        {
+          href: "https://docs.pytest.org/en/latest/",
+          description: "PyTest",
+          src: logos.pytest,
+        },
+        {
+          href: "https://vuejs.org/",
+          description: "Vue.js",
+          src: logos.vue,
+          width: 40,
+        },
+      ]}
+    />
+    <p>Generates a report on the organization’s website, Facebook page, and SoundCloud account activity by integrating with Google Analytics and Facebook API; Data is stored in a JSON file so current week’s data can be compared with last week’s. A web form is provided for the user to enter SoundCloud data not available in their API.</p>
+    <Screenshots
+      images={[
+        {
+          src: psTests,
+          description: "Tests for the Pepperplate Crawler",
+        },
+        {
+          src: psFrontEnd1,
+          description: "Initial process",
+        },
+        {
+          src: psFrontEnd3,
+          description: "Exporting recipes",
+        },
+      ]}
+    />
+    <h3>Store Switchover</h3>
+    <BuiltWith
+      items={[
+        {
+          href: "https://www.python.org/",
+          description: "Python",
+          src: logos.python,
+        },
+        {
+          href: "https://docs.pytest.org/en/latest/",
+          description: "PyTest",
+          src: logos.pytest,
+        },
+        {
+          href: "https://www.pyinstaller.org/",
+          description: "PyInstaller",
+        },
+        {
+          href: "https://typer.tiangolo.com/",
+          description: "Typer",
+          src: logos.typer
         },
       ]}
     />
     <p>
-      The Pepperplate Snipper exports recipes in someone's Pepperplate account as
-      JSON and/or PNG screenshots through web-scraping &middot;&nbsp;
-      <a href="https://github.com/paxcodes/peppsnipp" class="linkRegular">
-        GitHub
-      </a>
+      Automatically adds products to the `Featured Products` category and removes `expired` products on the first day of the month by integrating with the DonorDirect’s StudioEnterprise API; Built with Python (Typer, PyTest, PyInstaller) and designed to run by crontab on CentOS 7 server every first day of the month at midnight.
     </p>
     <Screenshots
       images={[
